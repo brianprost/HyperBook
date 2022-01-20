c9c076205d1a811bc0af28de3a6b29db3ce8404e
Brian Prost <brian.s.prost@gmail.com>
2022-01-20 12:18:24 -0600

    styling of pricing page

---
+++ components/RouteOptionCard.component.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const RouteOptionCard = (props) => {
+  return (
+    <div>
+      <div className="relative flex flex-col p-8 bg-zinc-300 rounded-xl border-4 border-red-700">
+        <div className="flex-1 font-hahmlet font-extrabold">
+          <h3 className="text-5xl underline text-center tracking-tighter mb-10">
+            {props.timeOfDay}
+          </h3>
+          <p className="flex items-baseline my-4 justify-center">
+            <span className="text-3xl font-extrabold tracking-tight p-2 text-center border-4 border-red-700 rounded-xl w-2/3 bg-zinc-100">
+              {props.departureTime}
+            </span>
+          </p>
+          <p className="flex items-baseline my-4 justify-center">
+            <span className="text-3xl font-extrabold tracking-tight p-2 text-center border-4 border-red-700 rounded-xl w-2/3 bg-zinc-100">
+              {props.arrivalTime}
+            </span>
+          </p>
+        </div>
+        <div className="mt-6 rounded-lg">
+          <a
+            href="#"
+            type="highlight"
+            className=" w-full items-center block px-10 py-3.5 text-xl font-hahmlet font-bold text-center transition duration-500 ease-in-out transform border-2 border-white shadow-md rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 bg-zinc-200
+        "
+          >
+            {props.tripPrice}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { RouteChoicespage } from "../components/RouteChoicespage.component";
 
 export default function Home() {
-  return <></>;
+  return <>
+  <Head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+<link href="https://fonts.googleapis.com/css2?family=Hahmlet&display=swap" rel="stylesheet" />
+
+  </Head>
+  <RouteChoicespage></RouteChoicespage>
+  </>;
 }
