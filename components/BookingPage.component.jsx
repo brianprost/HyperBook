26e4d1bddac4cccc659e3ab7f699058522d14a6d
Brian Prost <brian.s.prost@gmail.com>
2022-01-22 16:54:10 -0600

    account page

--- components/BookingPage.component.jsx
+++ components/BookingPage.component.jsx
@@ -7,11 +7,11 @@ export const BookingPage = () => {
     <section id="book">
       <div className="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-24 max-w-7xl">
         <div className="grid w-full gap-12 mx-auto lg:grid-cols-4">
-        {cities.map((city, index) => (
-          <CityOption city={city.city} key={index} />
-        ))}
+          {cities.map((city, index) => (
+            <CityOption city={city.city} key={index} />
+          ))}
         </div>
       </div>
-      </section>
+    </section>
   );
 };
--- components/RouteOptionCard.component.jsx
+++ components/RouteOptionCard.component.jsx
@@ -6,11 +6,11 @@ export const RouteOptionCard = (props) => {
     <div>
       <div className="relative flex flex-col p-8 bg-zinc-300 rounded-2xl border-[6px] border-red-700">
         <div className="flex-1 ">
-          <h3 className="text-5xl underline font-sans font-black text-center mb-10">
+          <h3 className="text-5xl underline font-Montserrat text-center tracking-tighter mb-10">
             {props.timeOfDay}
           </h3>
           <p className="flex items-baseline my-4 justify-center">
-            <span className="text-3xl font-mono font-bold tracking-wide p-2 text-center border-2 border-red-700 rounded-xl w-2/3 bg-zinc-100">
+            <span className="text-3xl font-mono tracking-wide p-2 text-center border-2 border-red-700 rounded-xl w-2/3 bg-zinc-100">
               {props.departureTime}
             </span>
           </p>
@@ -18,7 +18,7 @@ export const RouteOptionCard = (props) => {
             <ArrowDownIcon className="h-6 w-6 text-hyperblue" />
           </div>
           <p className="flex items-baseline my-4 justify-center">
-            <span className="text-3xl font-mono font-bold tracking-wide p-2 text-center border-2 border-red-700 rounded-xl w-2/3 bg-zinc-100">
+            <span className="text-3xl font-mono tracking-wide p-2 text-center border-2 border-red-700 rounded-xl w-2/3 bg-zinc-100">
               {props.arrivalTime}
             </span>
           </p>
@@ -27,7 +27,7 @@ export const RouteOptionCard = (props) => {
           <a
             href="#"
             type="highlight"
-            className=" w-full items-center block px-10 py-3.5 text-xl font-sans font-extrabold text-center transition duration-400 ease-in-out transform border-2 border-white shadow-md rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 bg-zinc-200 hover:bg-red-700 hover:text-zinc-200
+            className=" w-full items-center block px-10 py-3.5 text-xl font-mono font-extrabold text-center transition duration-400 ease-in-out transform border-2 border-white shadow-md rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 bg-zinc-200 hover:bg-red-700 hover:text-zinc-200
         "
           >
             {props.tripPrice}
