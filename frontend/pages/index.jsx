364c7a507f9d4404a63593410b8d17e994c8c8fe
Brian Prost <brian.s.prost@gmail.com>
2022-02-06 17:06:25 -0800

    migrated big image to webp

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -7,23 +7,23 @@ const Home = () => {
   const router = useRouter();
 
   return (
-    <section>
+    <section className="">
       <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
         <div className="flex flex-wrap items-center mx-auto max-w-7xl">
           <div className="w-full lg:max-w-lg lg:w-1/2 rounded-xl">
             <div>
               <div className="relative w-full max-w-lg">
-                <div className="relative">
+                <div className="relative drop-shadow-lg">
                   <img
-                    className="object-cover object-center mx-auto "
+                    className="object-cover object-center mx-auto animate-zoomInLeft "
                     alt="hero"
-                    src="./img/hyperbook-icon.png"
+                    src="./img/hyperbook-icon.webp"
                   />
                 </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0">
+          <div className="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0 animate-fadeInUp">
             <h1 className="mb-8 text-4xl font-bold text-hyperblue md:text-7xl drop-shadow-md">
               HYPERBOOK
             </h1>
@@ -33,16 +33,12 @@ const Home = () => {
             </p>
             <div className="mt-0 lg:mt-6 max-w-7xl sm:flex">
               <div className="mt-3 rounded-lg sm:mt-0">
-                {/* <Link href={"/book"}>
-                  <a> */}
                 <button
                   className="items-center block px-10 py-4 text-base font-bold tracking-wide text-center text-hypertan shadow-lg transition duration-500 ease-in-out transform bg-hyperblue rounded-xl hover:bg-blue-900"
                   onClick={() => router.push("/book")}
                 >
                   Book Now
                 </button>
-                {/* </a>
-                </Link> */}
               </div>
               <div className="mt-3 rounded-lg sm:mt-0 sm:ml-3">
                 <Link href="/login">
